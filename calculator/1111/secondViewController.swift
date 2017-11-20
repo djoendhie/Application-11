7d32d2ac66a38862c7ac659766365dc78497bc5a
Djoendhie <ihaandjoe68@Gmail.com>
2017-11-20 11:22:01 +0700

    Calculator

---
+++ calculator/1111/secondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  secondViewController.swift
+//  1111
+//
+//  Created by SMK IDN MI on 10/10/17.
+//  Copyright © 2017 Be Dev. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
